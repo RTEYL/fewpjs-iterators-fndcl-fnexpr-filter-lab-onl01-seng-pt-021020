1998010053a1f6a47daaf0c61a8a8ea9d3b0af7a
tyler jones <rteyl1690@gmail.com>
2020-09-01 01:30:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function findMatching(arr, str){
     return s.toLowerCase() === str.toLowerCase()
   })
 }
+
+function fuzzyMatch(arr, str){
+  return arr.filter(s => {
+    return s.match(str)
+  })
+}
